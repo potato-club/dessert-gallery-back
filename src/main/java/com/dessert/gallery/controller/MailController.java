63733d9af20868a8a7073b4bd52aa80dbaa41600
cheomuk <pkw9815@naver.com>
2023-05-02 14:52:45 +0900

    Add Swagger Annotation

--- src/main/java/com/dessert/gallery/controller/MailController.java
+++ src/main/java/com/dessert/gallery/controller/MailController.java
@@ -1,6 +1,8 @@
 package com.dessert.gallery.controller;
 
 import com.dessert.gallery.service.Interface.EmailService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +14,26 @@ import java.io.UnsupportedEncodingException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/users/mail")
+@Api(tags = {"Mail Authorization Controller"})
 public class MailController {
 
     private final EmailService emailService;
 
+    @Operation(summary = "Gmail 인증 메일 발송 API")
     @PostMapping("/gmail")
     public ResponseEntity<String> sendGmail(String recipientEmail) throws MessagingException, UnsupportedEncodingException {
         emailService.sendGmail(recipientEmail);
         return ResponseEntity.ok("인증 메일이 발송되었습니다.");
     }
 
+    @Operation(summary = "Naver 인증 메일 발송 API")
     @PostMapping("/naver")
     public ResponseEntity<String> sendNaver(String recipientEmail) throws MessagingException, UnsupportedEncodingException {
         emailService.sendNaver(recipientEmail);
         return ResponseEntity.ok("인증 메일이 발송되었습니다.");
     }
 
+    @Operation(summary = "인증 코드 확인 API")
     @PostMapping("/verify")
     public ResponseEntity<String> verifyEmail(String key, HttpServletResponse response) {
         emailService.verifyEmail(key, response);
--- src/main/java/com/dessert/gallery/controller/S3Controller.java
+++ src/main/java/com/dessert/gallery/controller/S3Controller.java
@@ -1,40 +1,48 @@
 package com.dessert.gallery.controller;
 
 import com.dessert.gallery.service.S3.S3Service;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLEncoder;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/s3")
+@Api(tags = {"AWS S3 Upload & Download Controller"})
 public class S3Controller {
 
     private final S3Service s3Service;
 
+    @Operation(summary = "S3 Upload API")
     @PostMapping("/upload")
     public ResponseEntity<String> s3Upload(List<MultipartFile> files) throws IOException {
         s3Service.uploadImages(files);
         return ResponseEntity.ok("사진이 정상적으로 저장되었습니다.");
     }
 
+    @Operation(summary = "S3 Download API")
     @GetMapping("/download")
-    public ResponseEntity<ByteArrayResource> s3Download(@RequestParam String key) {
-        try{
+    public ResponseEntity<InputStreamResource> s3Download(@RequestParam String key) {
+        try {
             byte[] data = s3Service.downloadImage(key);
-            ByteArrayResource resource = new ByteArrayResource(data);
+            InputStream inputStream = new ByteArrayInputStream(data);
+            InputStreamResource resource = new InputStreamResource(inputStream);
             return ResponseEntity
                     .ok()
                     .contentLength(data.length)
                     .header("Content-type", "application/octet-stream")
                     .header("Content-Disposition", "attachment; filename=" +
-                            URLEncoder.encode(key, "euc-kr"))
+                            URLEncoder.encode(key, "UTF-8"))
                     .body(resource);
         } catch (IOException ex) {
             return ResponseEntity.badRequest().contentLength(0).body(null);
