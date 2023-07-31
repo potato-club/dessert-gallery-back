43721b27ad8d8e219eeeaa0fbb5beb191d673593
cheomuk <pkw9815@naver.com>
2023-08-01 00:07:15 +0900

    Revise JWT Exception Logic

--- src/main/java/com/dessert/gallery/config/SecurityConfig.java
+++ src/main/java/com/dessert/gallery/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 // 정보수정은 USER, MANAGER, ADMIN 권한이 필요하도록 설정
                 .antMatchers(HttpMethod.PUT,"/users/**").hasAnyAuthority("USER", "MANAGER")
                 // 로그아웃, 마이페이지 API 또한 권한이 필요하도록 설정
-                .antMatchers("/users/logout").hasAnyAuthority("USER", "MANAGER")
+                .antMatchers(HttpMethod.GET,"/users/logout").hasAnyAuthority("USER", "MANAGER")
                 .antMatchers(HttpMethod.GET,"/users").hasAnyAuthority("USER", "MANAGER")
                 // 나머지 요청에 대해서는 권한 제한 없이 호출 가능하도록 설정
                 .anyRequest().permitAll()
--- src/main/java/com/dessert/gallery/jwt/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/dessert/gallery/jwt/JwtAuthenticationTokenFilter.java
@@ -90,7 +90,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
         json.put("code", errorCode.getCode());
         json.put("message", errorCode.getMessage());
         response.getWriter().print(json);
-        response.getWriter().flush();
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 
     private void setAuthentication(String token) {
