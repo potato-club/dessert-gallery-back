f1ddfd9ce4fdf7e6c073103b8e02976e57101a39
cheomuk <pkw9815@naver.com>
2023-08-21 16:32:06 +0900

    Revise SearchFilter

--- src/main/java/com/dessert/gallery/controller/StoreListController.java
+++ src/main/java/com/dessert/gallery/controller/StoreListController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
@@ -22,13 +23,13 @@ public class StoreListController {
 
     @Operation(summary = "필터 적용/미적용 가게 목록 조회 API")
     @GetMapping("/stores")
-    public List<StoreListResponseDto> getStoreList(@ModelAttribute StoreSearchDto storeSearchDto) {
-        return storeListService.getStoreList(storeSearchDto);
+    public List<StoreListResponseDto> getStoreList(@ModelAttribute StoreSearchDto storeSearchDto, HttpServletRequest request) {
+        return storeListService.getStoreList(storeSearchDto, request);
     }
 
     @Operation(summary = "필터 적용/미적용 리뷰 목록 조회 API")
     @GetMapping("/reviews")
-    public List<StoreReviewDto> getReviewList(@ModelAttribute StoreSearchDto storeSearchDto) {
-        return storeListService.getReviewList(storeSearchDto);
+    public List<StoreReviewDto> getReviewList(@ModelAttribute StoreSearchDto storeSearchDto, HttpServletRequest request) {
+        return storeListService.getReviewList(storeSearchDto, request);
     }
 }
--- src/main/java/com/dessert/gallery/service/Interface/StoreListService.java
+++ src/main/java/com/dessert/gallery/service/Interface/StoreListService.java
@@ -5,11 +5,12 @@ import com.dessert.gallery.dto.store.list.StoreReviewDto;
 import com.dessert.gallery.dto.store.list.StoreSearchDto;
 import com.querydsl.core.Tuple;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface StoreListService {
 
-    List<StoreListResponseDto> getStoreList(StoreSearchDto storeSearchDto);
+    List<StoreListResponseDto> getStoreList(StoreSearchDto storeSearchDto, HttpServletRequest request);
 
-    List<StoreReviewDto> getReviewList(StoreSearchDto storeSearchDto);
+    List<StoreReviewDto> getReviewList(StoreSearchDto storeSearchDto, HttpServletRequest request);
 }
