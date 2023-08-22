75f7f933f5cb34fc939f4d44b684d289410883cb
CJ-Park <king01286@naver.com>
2023-08-22 14:15:53 +0900

    #3 [fix] 가게 공지글 이미지 삭제에 따른 로직 수정

--- src/main/java/com/dessert/gallery/dto/notice/NoticeResponseDto.java
+++ src/main/java/com/dessert/gallery/dto/notice/NoticeResponseDto.java
@@ -1,14 +1,10 @@
 package com.dessert.gallery.dto.notice;
 
-import com.dessert.gallery.dto.file.FileDto;
 import com.dessert.gallery.entity.NoticeBoard;
 import com.dessert.gallery.enums.NoticeType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -17,14 +13,9 @@ public class NoticeResponseDto {
     private String content;
     @Schema(description = "공지글 타입 (NOTICE / EVENT)")
     private NoticeType type;
-    @Schema(description = "공지글 이미지 파일")
-    private List<FileDto> images;
 
     public NoticeResponseDto(NoticeBoard noticeBoard) {
         this.content = noticeBoard.getContent();
         this.type = noticeBoard.getType();
-        this.images = noticeBoard.getImages().isEmpty() ? null : noticeBoard.getImages().stream()
-                .map(FileDto::new)
-                .collect(Collectors.toList());
     }
 }
