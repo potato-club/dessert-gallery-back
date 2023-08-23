a10eb9c69eaf98fc4b66b057d7747510fe720798
CJ-Park <king01286@naver.com>
2023-08-23 14:06:44 +0900

    #3 [fix] board의 subtitle 제거 및 store에 info 컬럼 추가, 게시글 반환 시 태그 List<String>으로 변환해서 반환

--- src/main/java/com/dessert/gallery/dto/board/BoardResponseDto.java
+++ src/main/java/com/dessert/gallery/dto/board/BoardResponseDto.java
@@ -5,7 +5,11 @@ import com.dessert.gallery.entity.StoreBoard;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Getter
@@ -13,22 +17,30 @@ import java.util.stream.Collectors;
 public class BoardResponseDto {
     @Schema(description = "게시글 제목")
     private String title;
-    @Schema(description = "게시글 부제목")
-    private String subtitle;
     @Schema(description = "게시글 내용")
     private String content;
     @Schema(description = "게시글 이미지 파일")
     private List<FileDto> images;
     @Schema(description = "게시글 해시태그")
-    private String tags;
+    private List<String> tags = new ArrayList<>();
 
     public BoardResponseDto(StoreBoard board) {
         this.title = board.getTitle();
-        this.subtitle = board.getSubtitle();
         this.content = board.getContent();
         this.images = board.getImages().isEmpty() ? null : board.getImages().stream()
                 .map(FileDto::new)
                 .collect(Collectors.toList());
-        this.tags = board.getTags();
+        this.tags = convertTags(board.getTags());
+    }
+
+    private List<String> convertTags(String tags) {
+        Pattern TAG_PATTERN = Pattern.compile("#([^#]*)");
+        List<String> result = new ArrayList<>();
+
+        Matcher matcher = TAG_PATTERN.matcher(tags);
+        while (matcher.find()) {
+            result.add(matcher.group(1));
+        }
+        return result;
     }
 }
