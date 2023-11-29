b28b3be1349599f9e47011b8db059a453c3f36e9
CJ-Park <king01286@naver.com>
2023-11-29 13:56:20 +0900

    #47 [fix] 사장님 마이페이지 공지사항 조회시 키워드 검색 쿼리 추가 및 해당 메서드 QueryDSL 구조로 변경

--- src/main/java/com/dessert/gallery/repository/NoticeBoardRepository.java
+++ src/main/java/com/dessert/gallery/repository/NoticeBoardRepository.java
@@ -2,7 +2,6 @@ package com.dessert.gallery.repository;
 
 import com.dessert.gallery.entity.NoticeBoard;
 import com.dessert.gallery.entity.Store;
-import com.dessert.gallery.enums.NoticeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +9,6 @@ import java.util.List;
 
 @Repository
 public interface NoticeBoardRepository extends JpaRepository<NoticeBoard, Long> {
-    List<NoticeBoard> findByStoreAndDeletedIsFalse(Store store);
-    List<NoticeBoard> findByStoreAndDeletedIsFalseAndType(Store store, NoticeType type);
     List<NoticeBoard> findByStoreAndDeletedFalseAndExposedTrue(Store store);
     NoticeBoard findByIdAndDeletedIsFalse(Long id);
 }
--- src/main/java/com/dessert/gallery/service/Impl/NoticeBoardServiceImpl.java
+++ src/main/java/com/dessert/gallery/service/Impl/NoticeBoardServiceImpl.java
@@ -3,6 +3,7 @@ package com.dessert.gallery.service.Impl;
 import com.dessert.gallery.dto.notice.NoticeListDto;
 import com.dessert.gallery.dto.notice.NoticeRequestDto;
 import com.dessert.gallery.entity.NoticeBoard;
+import com.dessert.gallery.entity.QNoticeBoard;
 import com.dessert.gallery.entity.Store;
 import com.dessert.gallery.entity.User;
 import com.dessert.gallery.enums.NoticeType;
@@ -12,14 +13,20 @@ import com.dessert.gallery.repository.NoticeBoardRepository;
 import com.dessert.gallery.service.Interface.NoticeBoardService;
 import com.dessert.gallery.service.Interface.StoreService;
 import com.dessert.gallery.service.Interface.UserService;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.dessert.gallery.error.ErrorCode.*;
@@ -29,6 +36,7 @@ import static com.dessert.gallery.error.ErrorCode.*;
 @Transactional
 @RequiredArgsConstructor
 public class NoticeBoardServiceImpl implements NoticeBoardService {
+    private final JPAQueryFactory jpaQueryFactory;
     private final NoticeBoardRepository noticeRepository;
     private final StoreService storeService;
     private final UserService userService;
@@ -45,23 +53,44 @@ public class NoticeBoardServiceImpl implements NoticeBoardService {
     }
 
     // 사장님 마이페이지 공지사항 리스트 출력
+    // no-offset 방식 무한스크롤
     @Override
-    public List<NoticeListDto> getNoticesByOwner(int typeKey, HttpServletRequest request) {
+    public Slice<NoticeListDto> getNoticesByOwner(int typeKey, String keyword, Long last, HttpServletRequest request) {
         User owner = userService.findUserByToken(request);
         Store store = storeService.getStoreByUser(owner);
+        NoticeType type = NoticeType.findWithKey(typeKey);
         if (store == null) throw new NotFoundException("존재하지 않는 가게", NOT_FOUND_EXCEPTION);
         if (owner == null || owner != store.getUser())
             throw new UnAuthorizedException("401 권한 없음", NOT_ALLOW_WRITE_EXCEPTION);
-        List<NoticeBoard> notices;
-        NoticeType type = NoticeType.findWithKey(typeKey);
-        if (type == NoticeType.ALL) {
-            notices = noticeRepository.findByStoreAndDeletedIsFalse(store);
-        } else {
-            notices = noticeRepository.findByStoreAndDeletedIsFalseAndType(store, type);
-        }
-        if (notices == null) throw new NotFoundException("게시물 없음", NOT_FOUND_EXCEPTION);
 
-        return notices.stream().map(NoticeListDto::new).collect(Collectors.toList());
+        Pageable pageable = PageRequest.ofSize(10);
+
+        BooleanBuilder whereQuery = new BooleanBuilder();
+        whereQuery.and(QNoticeBoard.noticeBoard.deleted.isFalse()); // 삭제 안된 공지
+        if (type != NoticeType.ALL) whereQuery.and(QNoticeBoard.noticeBoard.type.eq(type));
+        if (keyword != null) whereQuery.and(QNoticeBoard.noticeBoard.title.contains(keyword)); // 키워드 검색 조건
+        if (last != null) whereQuery.and(QNoticeBoard.noticeBoard.id.lt(last)); // no-offset 을 위한 마지막 id 값 확인
+
+        JPAQuery<NoticeBoard> query = jpaQueryFactory
+                .select(QNoticeBoard.noticeBoard).from(QNoticeBoard.noticeBoard)
+                .where(whereQuery.and(QNoticeBoard.noticeBoard.store.eq(store)))
+                .orderBy(QNoticeBoard.noticeBoard.createdDate.desc())
+                .limit(pageable.getPageSize() + 1);
+
+        return transType(query.fetch(), pageable);
+    }
+
+    // List to Slice
+    // NoticeBoard to Dto
+    private Slice<NoticeListDto> transType(List<NoticeBoard> list, Pageable pageable) {
+        boolean hasNext = false;
+
+        if (list.size() > pageable.getPageSize()) {
+            hasNext = true;
+            list.remove(pageable.getPageSize());
+        }
+        Slice<NoticeBoard> slice = new SliceImpl<>(list, pageable, hasNext);
+        return slice.map(NoticeListDto::new);
     }
 
     @Override
