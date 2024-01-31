7b60bb22949ee05b8d5d8067a603cac9ad7f8f9f
CJ-Park <king01286@naver.com>
2024-01-31 22:41:09 +0900

    #12 [feat] 예약 스케줄에 체크 기능 추가

--- src/main/java/com/dessert/gallery/service/Interface/ScheduleService.java
+++ src/main/java/com/dessert/gallery/service/Interface/ScheduleService.java
@@ -14,6 +14,7 @@ import java.util.List;
 public interface ScheduleService {
     void addSchedule(ScheduleRequestDto requestDto, HttpServletRequest request);
     void addReservation(ReservationRequestDto requestDto, HttpServletRequest request);
+    void toggleSchedule(Long scheduleId, HttpServletRequest request);
     void removeSchedule(Long scheduleId, HttpServletRequest request);
     List<Schedule> getSchedules(Calendar calendar, LocalDateTime startDate, LocalDateTime endDate);
     List<Schedule> getSchedulesForOwner(Calendar calendar, LocalDateTime startDate, LocalDateTime endDate);
