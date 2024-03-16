a851fdd4cfee0e7a59770c0ce161789d9d22a266
cheomuk <pkw9815@naver.com>
2024-03-16 09:42:48 +0900

    #47 [Refactoring] Separate JPA queries and custom queries.

--- src/main/java/com/dessert/gallery/repository/BlackList/BlackListRepository.java
+++ src/main/java/com/dessert/gallery/repository/BlackList/BlackListRepository.java
@@ -1,13 +1,9 @@
 package com.dessert.gallery.repository.BlackList;
 
-import com.dessert.gallery.dto.blacklist.BlackListResponseDto;
 import com.dessert.gallery.entity.BlackList;
 import com.dessert.gallery.entity.Store;
 import com.dessert.gallery.entity.User;
-import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface BlackListRepository extends JpaRepository<BlackList, Long>, BlackListRepositoryCustom {
 
@@ -16,8 +12,4 @@ public interface BlackListRepository extends JpaRepository<BlackList, Long>, Bla
     boolean existsByUserAndStoreAndDeletedIsFalse(User user, Store store);
 
     boolean existsByUserAndStoreAndDeletedIsTrue(User user, Store store);
-
-    List<BlackListResponseDto> getBlackListInStore(int page, Store store);
-
-    JPAQuery<Long> countByStoreAndDeletedIsFalse(Store store);
 }
--- src/main/java/com/dessert/gallery/repository/Subscribe/SubscribeRepository.java
+++ src/main/java/com/dessert/gallery/repository/Subscribe/SubscribeRepository.java
@@ -1,13 +1,9 @@
 package com.dessert.gallery.repository.Subscribe;
 
-import com.dessert.gallery.dto.follow.FollowResponseDto;
 import com.dessert.gallery.entity.Store;
 import com.dessert.gallery.entity.Subscribe;
 import com.dessert.gallery.entity.User;
-import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long>, SubscribeRepositoryCustom {
 
@@ -20,8 +16,4 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Long>, Sub
     boolean existsByStoreAndUser(Store store, User user);
 
     Long countAllByStoreAndDeletedIsFalse(Store store);
-
-    List<FollowResponseDto> findDistinctFollowResponseDtoByEmail(int page, String email);
-
-    JPAQuery<Long> countByUserEmailAndDeletedIsFalseAndNothingBlackList(String email);
 }
