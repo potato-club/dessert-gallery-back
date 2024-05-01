b0b28b4d6dc002fa7e0f12999ae1686c49e01ceb
cheomuk <pkw9815@naver.com>
2024-05-01 15:35:58 +0900

    [Fix] Fixed addBlackList Options.

--- src/main/java/com/dessert/gallery/service/Impl/BlackListServiceImpl.java
+++ src/main/java/com/dessert/gallery/service/Impl/BlackListServiceImpl.java
@@ -52,7 +52,10 @@ public class BlackListServiceImpl implements BlackListService {
             Subscribe subscribe = subscribeRepository.findByUserAndStore(customer, store);
 
             blackList.setDeleted(false);
-            subscribe.setDeleted(true);
+
+            if (subscribe != null) {
+                subscribe.setDeleted(true);
+            }
 
         } else if (!blackListRepository.existsByUserAndStore(customer, store)) {
             // 블랙리스트에 등록된 적이 없었던 경우
